90e1d2ed3eb452b63f5ffe12ddba5b5858a37233
Leon A. Okida Gonçalves <laog19@inf.ufpr.br>
2022-08-06 18:25:42 -0300

    Function to choose bet

--- playerD.py
+++ playerD.py
@@ -1,25 +1,64 @@
 import socket
+import sys
+import os
 
+# System variables
 IP = "127.0.0.1"
 PORT = 5003
 
-ME = "D"
+NAME = "D"
 ADDREC = 5002
 ADDSEND = 5000
 
-relayBaton = False
+# Controls relay baton
+relayBaton = True
 
+# Configures DGRAM socket
 mySocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 mySocket.bind((IP, PORT))
 
+# Game variables
+chips = 20
+currentGambler = ""
+betNames = ["Par", "Trio", "2 Pares", "Full House", "Seq. Baixa", "Seq. Alta", "Quadra", "General"]
+betValues = [2, 3, 4, 5, 7, 7, 10, 15]
+
+# Function that shows all possible bets and gets choice
+def chooseBet():
+    print("Escolha uma das seguintes apostas (digite o número):")
+    index = 1
+    for bet in betNames:
+        print(index, "- ", bet)
+        index+=1
+
+    choice = input()
+    return (int(choice) - 1)
+
 while True:
     if relayBaton:
-        print("Tenho bastão")
-        input()
+        os("clear")
+
+        # Makes choice
+        choice = chooseBet()
+        os("clear")
+
+        # Sends to next
+        marker = b'9'
+        msgType = b'2'
+        size = b'2'
+        data = str.encode(NAME + str(choice))
+        parity = b'0'
+        message = marker + msgType + size + data + parity
+        mySocket.sendto(message, (IP, ADDSEND))
+
+        # Awaits return
+
+        # Decides if it bets or passes to gambler
+
+        # Sends baton
         mySocket.sendto(b"bastao", (IP, ADDSEND))
         relayBaton = False
     else:
         data, addr = mySocket.recvfrom(1024)
         if addr[1] == ADDREC:
-            if data == b'bastao':
-                relayBaton = True
+            data = data.decode("utf-8")
